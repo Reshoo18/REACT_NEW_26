c1975c36fb612ac784b8b268351936427e67c010
Reshoo-18 <reshooranjan7@gmail.com>
2026-06-06 23:42:14 +0530

    ReactDom

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var h1=React.createElement("h1",null,"hello from react")
 
-console.log(h1)
+var parent=document.getElementById("#parent")
+
+var root=ReactDOM.createRoot(parent)
+
+root.render(h1)
+
+
+
