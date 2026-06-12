92184ca88713383339b622dc4b78ad2705ca3401
Reshoo-18 <reshooranjan7@gmail.com>
2026-06-12 23:39:57 +0530

    variable

--- REACT_LEARNING/src/App.jsx
+++ REACT_LEARNING/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
+   
     <div>
-      <h1>Hello H1</h1>
+       const a=10;
+      <h1>Hello H1 {a}</h1>
       <h2>Hello H2</h2>
     </div>
   )
