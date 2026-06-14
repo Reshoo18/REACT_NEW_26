a3ff8e93cd636e308ad398074d4cf247551cdecf
Reshoo-18 <reshooranjan7@gmail.com>
2026-06-14 13:28:33 +0530

    components

--- REACT_LEARNING/src/App.jsx
+++ REACT_LEARNING/src/App.jsx
@@ -1,29 +1,11 @@
-import React, { useState } from 'react'
+import Navbar from "./components/Navbar"
 
-const App = () => {
-
-  const  [a ,setA]=useState(20)
-
-  const ChangeA=()=>{
-    console.log("CHal gya");
-    setA(a+10)
-  }
-   const ChangeB=()=>{
-    console.log("CHal gya");
-    if(a>0){
-      setA(a-10)
-    }
-  }
+function App() {
   return (
-   
-    <div>
-       
-      <h1>Hello H1 {a} </h1>
-     
-      <button onClick={ChangeA}>Increment</button>
-      <button onClick={ChangeB}>decrement</button>
-     
-    </div>
+    <>
+     <Navbar/>
+
+    </>
   )
 }
 
