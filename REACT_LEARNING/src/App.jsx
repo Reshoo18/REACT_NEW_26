e75b44721e8850b805546fec5a0b7512081bf4e2
Reshoo-18 <reshooranjan7@gmail.com>
2026-06-16 13:30:33 +0530

    props data pass

--- REACT_LEARNING/src/App.jsx
+++ REACT_LEARNING/src/App.jsx
@@ -2,10 +2,57 @@ import React from 'react'
 import Card from './components/Card'
 
 const App = () => {
-  const user="sarthak"
+ const users = [
+  {
+    id: 1,
+    name: "Aarav Sharma",
+    city: "Delhi",
+    age: 25,
+    profession: "Software Developer",
+    profilePhoto: "https://randomuser.me/api/portraits/men/1.jpg"
+  },
+  {
+    id: 2,
+    name: "Priya Verma",
+    city: "Mumbai",
+    age: 28,
+    profession: "UI/UX Designer",
+    profilePhoto: "https://randomuser.me/api/portraits/women/2.jpg"
+  },
+  {
+    id: 3,
+    name: "Rohan Gupta",
+    city: "Bangalore",
+    age: 30,
+    profession: "Data Analyst",
+    profilePhoto: "https://randomuser.me/api/portraits/men/3.jpg"
+  },
+  {
+    id: 4,
+    name: "Sneha Kapoor",
+    city: "Pune",
+    age: 26,
+    profession: "Digital Marketer",
+    profilePhoto: "https://randomuser.me/api/portraits/women/4.jpg"
+  },
+  {
+    id: 5,
+    name: "Kabir Singh",
+    city: "Chandigarh",
+    age: 32,
+    profession: "Product Manager",
+    profilePhoto: "https://randomuser.me/api/portraits/men/5.jpg"
+  }
+];
+
+
   return (
     <div>
-      <Card/>
+      <div className='p-6'>
+       {users.map(function(elem,idx){
+        return <Card key={idx} username={elem.name} city={elem.city} age={elem.age} profession={elem.profession} photo={elem.profilePhoto} />
+       })}
+      </div>
     </div>
   )
 }
--- REACT_LEARNING/src/components/Card.jsx
+++ REACT_LEARNING/src/components/Card.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-  <h1 className='text-3xl'>Card</h1>
+const Card = (props) => {
+  return ( 
+  <div className=' mr-7 bg-black text-white inline-block p-6 text-center rounded'>
+    <img className='ml-8 h-32 w-32 rounded-full mb-3' src={props.photo} alt="" />
+    <h1 className='text-2xl font-semibold mb-4'>{props.username} </h1>
+    <h4 className='text-blue-400'>{props.profession}</h4>
+    <h2> {props.city} , {props.age}</h2>
+    <button className= ' mt-5 bg-emerald-700 text-black px-4 py-2 rounded font-medium'>Add friend</button>
+  </div>
   )
 }
 
